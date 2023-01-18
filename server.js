c43c28a9e351659066653dc86cc9cd3b3cab9e6f
char <charlows@gmail.com>
2023-01-19 10:23:49 +1100

    resfresh fix

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/v1/content', contentRoutes);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
